49040dc04c354c7e2d55d4a84292bf0934829e81
Gaël Oberson <gael.oberson@he-arc.ch>
2015-03-31 12:06:48 +0200

    insertion du projet netbeans

---
+++ AppClientCompte/src/java/dbfactory/OracleConnections.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbfactory;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oracle.jdbc.pool.OracleDataSource;
+
+/**
+ *
+ * @author christop.francill
+ */
+public class OracleConnections {
+    private static OracleDataSource ods = null;
+    
+    private static final String USER = "prof_francillon";
+    private static final String PASSWORD = "prof_francillon";
+    private static final String HOST = "ne-ege-leto.ig.he-arc.ch";
+    private static final String PORT = "1521";
+    private static final String SID = "ens2";
+    
+    static ComboPooledDataSource cpds;
+    static{
+        ComboPooledDataSource cpds = new ComboPooledDataSource();
+        try {
+            cpds.setDriverClass( "oracle.jdbc.OracleDriver" ); //loads the jdbc driver            
+        } catch (PropertyVetoException ex) {
+            Logger.getLogger(OracleConnections.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        cpds.setJdbcUrl( "jdbc:oracle:thin:@" + HOST + ":" + PORT + ":" + SID);
+        cpds.setUser(USER);                                  
+        cpds.setPassword(PASSWORD); 
+        // the settings below are optional -- c3p0 can work with defaults
+        cpds.setMinPoolSize(5);                                     
+        cpds.setAcquireIncrement(5);
+        cpds.setMaxPoolSize(20);
+        cpds.setAutoCommitOnClose(false);
+    
+    }
+    
+    public static Connection getConnection() throws SQLException{
+        Connection c = cpds.getConnection();
+        System.out.println(c);
+        return c;
+    }
+}
